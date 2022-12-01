1e15fb8ac7adfbf98cf083e586672310de07c0ac
juanburg98 <juanburg98@gmail.com>
2022-12-01 19:46:01 -0300

    Ultimos arreglos

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ function Card () {
     return(
         <div className = "card_container">
             
-        <img src="https://d1k1kwopu3t53x.cloudfront.net/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/p/a/paz3622901-1.jpg" />
+        <img src=".\public\top-candy.jpg" />
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import CartWidget from "./CartWidget";
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <h2>Bag Store</h2>
-      <button className="buttons">Carteras</button>
-      <button className="buttons">Mochilas</button>
-      <button className="buttons">Billeteras</button>
+      <h2>Gaki Grow Shop</h2>
+      <button className="buttons">Fertilizantes</button>
+      <button className="buttons">Semillas</button>
+      <button className="buttons">Sustratos</button>
       <CartWidget />
     </div>
   );
